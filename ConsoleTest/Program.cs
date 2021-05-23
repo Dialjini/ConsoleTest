9337ac987bf9c0a5434eac20bfa1966f8e258338
Dialjini <danil_kustov@mail.ru>
2021-05-23 23:06:45 +0700

    Some models and parser

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,25 +1,35 @@
 ﻿using System;
+using System.Net;
+using System.IO;
 
 namespace ConsoleTest
 {
     class Program
     {
-        static void Controller()
+        static int Controller()
         {
             Console.WriteLine("Введите номер тендера:");
             try
             {
                 int number = Int32.Parse(Console.ReadLine()); // Явное приведение к Int32, если айди тендера в Int32
-            }                                                 // В задании не уточняется Int32 или Int64
+                return number;
+            }
             catch (FormatException)
             {
                 Console.WriteLine("Введеная строка не является числом."); // Если строка не является числом, рекурсивно вызываем контроллер
-                Controller();
+                return Controller();                                      // и возвращаем получившееся число.  
             }
         }
         static void Main(string[] args)
         {
-            Controller(); // Вызываем контроллер
+            int tenderId = Controller(); // Вызываем контроллер
+            using (var webClient = new WebClient())
+            {
+                // Выполняем запрос по адресу и получаем ответ в виде строки
+                var response = webClient.DownloadString(String.Format("https://market.mosreg.ru/Trade/ViewTrade/{0}", tenderId));
+                Parser parser = new Parser();
+                parser.Parse(response);
+            }
         }
     }
 }
